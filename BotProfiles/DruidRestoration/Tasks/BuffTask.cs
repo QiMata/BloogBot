d84493de8de30315fa9b10dc553e7d1282a4dd99
Jared Rhodes <jrhodes@qimata.com>
2025-07-18 20:28:25 -0400

    Implement restoration druid tasks

--- BotProfiles/DruidRestoration/Tasks/BuffTask.cs
+++ BotProfiles/DruidRestoration/Tasks/BuffTask.cs
@@ -1,13 +1,28 @@
-﻿using BotRunner.Interfaces;
+using BotRunner.Interfaces;
 using BotRunner.Tasks;
+using static BotRunner.Constants.Spellbook;
 
 namespace DruidRestoration.Tasks
 {
+    /// <summary>
+    /// Applies restoration druid buffs when out of combat.
+    /// </summary>
     internal class BuffTask(IBotContext botContext) : BotTask(botContext), IBotTask
     {
         public void Update()
         {
-            BotTasks.Pop();
+            if ((ObjectManager.Player.HasBuff(MarkOfTheWild) || !ObjectManager.Player.IsSpellReady(MarkOfTheWild)) &&
+                (ObjectManager.Player.HasBuff(Thorns) || !ObjectManager.Player.IsSpellReady(Thorns)))
+            {
+                BotTasks.Pop();
+                return;
+            }
+
+            if (!ObjectManager.Player.HasBuff(MarkOfTheWild))
+                ObjectManager.Player.CastSpell(MarkOfTheWild);
+
+            if (!ObjectManager.Player.HasBuff(Thorns))
+                ObjectManager.Player.CastSpell(Thorns);
         }
     }
 }
