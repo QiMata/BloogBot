6db51a3676400308d305e98cf1eb512e8cbbe682
Jared Rhodes <jrhodes@qimata.com>
2025-07-18 20:33:11 -0400

    Implement basic shaman rotations

--- BotProfiles/ShamanEnhancement/Tasks/PvPRotationTask.cs
+++ BotProfiles/ShamanEnhancement/Tasks/PvPRotationTask.cs
@@ -9,10 +9,26 @@ namespace ShamanEnhancement.Tasks
 
         public void Update()
         {
-            BotTasks.Pop();
+            if (!ObjectManager.Aggressors.Any())
+            {
+                BotTasks.Pop();
+                return;
+            }
+
+            if (Update(5))
+                return;
+
+            PerformCombatRotation();
         }
         public override void PerformCombatRotation()
         {
+            ObjectManager.Player.StopAllMovement();
+            ObjectManager.Player.Face(ObjectManager.GetTarget(ObjectManager.Player).Position);
+            ObjectManager.Player.StartMeleeAttack();
+
+            TryCastSpell(GroundingTotem, 0, int.MaxValue, ObjectManager.Aggressors.Any(a => a.IsCasting));
+            TryCastSpell(EarthShock, 0, 20, ObjectManager.GetTarget(ObjectManager.Player).IsCasting);
+            TryCastSpell(Stormstrike, 0, 5);
         }
     }
 }
