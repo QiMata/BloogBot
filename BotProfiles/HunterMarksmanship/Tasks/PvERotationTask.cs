2c20c47ccbd0ac10b9e67f7bb39651269314c2d1
Jared Rhodes <jrhodes@qimata.com>
2025-07-20 07:02:47 -0400

    Improve hunter rotations

--- BotProfiles/HunterMarksmanship/Tasks/PvERotationTask.cs
+++ BotProfiles/HunterMarksmanship/Tasks/PvERotationTask.cs
@@ -1,5 +1,8 @@
-﻿using BotRunner.Interfaces;
+﻿using BotRunner.Constants;
+using BotRunner.Interfaces;
 using BotRunner.Tasks;
+using GameData.Core.Enums;
+using static BotRunner.Constants.Spellbook;
 
 namespace HunterMarksmanship.Tasks
 {
@@ -10,11 +13,44 @@ namespace HunterMarksmanship.Tasks
 
         public void Update()
         {
-            BotTasks.Pop();
-        }
-        public override void PerformCombatRotation()
-        {
+            ObjectManager.Pet?.Attack();
+            if (!ObjectManager.Aggressors.Any())
+            {
+                BotTasks.Pop();
+                return;
+            }
 
+            if (ObjectManager.GetTarget(ObjectManager.Player) == null || ObjectManager.GetTarget(ObjectManager.Player).HealthPercent <= 0)
+                ObjectManager.Player.SetTarget(ObjectManager.Aggressors.First().Guid);
+
+            if (Update(34))
+                return;
+
+            ObjectManager.Player.StopAllMovement();
+
+            IWoWItem bow = ObjectManager.GetEquippedItem(EquipSlot.Ranged);
+            bool ranged = bow != null && ObjectManager.Player.Position.DistanceTo(ObjectManager.GetTarget(ObjectManager.Player).Position) > 5 &&
+                           ObjectManager.Player.Position.DistanceTo(ObjectManager.GetTarget(ObjectManager.Player).Position) < 34;
+
+            if (ranged)
+            {
+                var target = ObjectManager.GetTarget(ObjectManager.Player);
+                TryCastSpell(HuntersMark, 0, 34, !target.HasDebuff(HuntersMark));
+                TryCastSpell(ConcussiveShot, 0, 34, !target.HasDebuff(ConcussiveShot));
+                if (!target.HasDebuff(SerpentSting))
+                    TryCastSpell(SerpentSting, 0, 34);
+                else if (ObjectManager.Aggressors.Count() > 1)
+                    TryCastSpell(MultiShot, 0, 34);
+                else if (ObjectManager.Player.ManaPercent > 60)
+                    TryCastSpell(ArcaneShot, 0, 34);
+                return;
+            }
+
+            // melee fallback
+            TryCastSpell(MongooseBite, 0, 5);
+            TryCastSpell(WingClip, 0, 5, !ObjectManager.GetTarget(ObjectManager.Player).HasDebuff(WingClip));
+            TryCastSpell(RaptorStrike, 0, 5);
         }
+        public override void PerformCombatRotation() => Update();
     }
 }
--- BotProfiles/HunterSurvival/Tasks/PvERotationTask.cs
+++ BotProfiles/HunterSurvival/Tasks/PvERotationTask.cs
@@ -1,5 +1,8 @@
-﻿using BotRunner.Interfaces;
+using BotRunner.Constants;
+using BotRunner.Interfaces;
 using BotRunner.Tasks;
+using GameData.Core.Enums;
+using static BotRunner.Constants.Spellbook;
 
 namespace HunterSurvival.Tasks
 {
@@ -9,10 +12,43 @@ namespace HunterSurvival.Tasks
 
         public void Update()
         {
-            BotTasks.Pop();
+            ObjectManager.Pet?.Attack();
+            if (!ObjectManager.Aggressors.Any())
+            {
+                BotTasks.Pop();
+                return;
+            }
+
+            if (ObjectManager.GetTarget(ObjectManager.Player) == null || ObjectManager.GetTarget(ObjectManager.Player).HealthPercent <= 0)
+                ObjectManager.Player.SetTarget(ObjectManager.Aggressors.First().Guid);
+
+            if (Update(34))
+                return;
+
+            ObjectManager.Player.StopAllMovement();
+            IWoWItem ranged = ObjectManager.GetEquippedItem(EquipSlot.Ranged);
+            bool canShoot = ranged != null && ObjectManager.Player.Position.DistanceTo(ObjectManager.GetTarget(ObjectManager.Player).Position) > 5 &&
+                             ObjectManager.Player.Position.DistanceTo(ObjectManager.GetTarget(ObjectManager.Player).Position) < 34;
+
+            if (canShoot)
+            {
+                var target = ObjectManager.GetTarget(ObjectManager.Player);
+                TryCastSpell(HuntersMark, 0, 34, !target.HasDebuff(HuntersMark));
+                TryCastSpell(ConcussiveShot, 0, 34, !target.HasDebuff(ConcussiveShot));
+                if (!target.HasDebuff(SerpentSting))
+                    TryCastSpell(SerpentSting, 0, 34);
+                else if (ObjectManager.Aggressors.Count() > 1)
+                    TryCastSpell(MultiShot, 0, 34);
+                else if (ObjectManager.Player.ManaPercent > 60)
+                    TryCastSpell(ArcaneShot, 0, 34);
+                return;
+            }
+
+            TryCastSpell(MongooseBite, 0, 5);
+            TryCastSpell(WingClip, 0, 5, !ObjectManager.GetTarget(ObjectManager.Player).HasDebuff(WingClip));
+            TryCastSpell(RaptorStrike, 0, 5);
         }
-        public override void PerformCombatRotation()
-        {
-        }
+
+        public override void PerformCombatRotation() => Update();
     }
 }
