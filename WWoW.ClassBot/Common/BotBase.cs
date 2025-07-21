7c0987884c96d71112fe558fc7b5cd1fbecabbf7
Jared Rhodes <jrhodes@qimata.com>
2025-07-21 17:19:35 -0400

    Moves bot profiles to ClassBot project
    Relocates bot profiles to the WWoW.ClassBot project for better organization.
    
    Updates the BotRunner project to remove the Nodes folder and adds a reference to the GameData.Core project.

--- WWoW.ClassBot/Common/BotBase.cs
+++ WWoW.ClassBot/Common/BotBase.cs
@@ -1,13 +1,20 @@
 using BotRunner;
-using BotRunner.Interfaces;
 using Communication;
+using GameData.Core.Interfaces;
 
 namespace BotProfiles.Common
 {
     internal abstract class BotBase : IBot
     {
+        protected BotBase(IObjectManager objectManager)
+        {
+            ObjectManager = objectManager;
+        }
+
         public abstract string Name { get; }
         public abstract string FileName { get; }
+
+        protected IObjectManager ObjectManager { get; }
 
         public virtual IClassContainer GetClassContainer(ActivityMemberState probe) =>
             new ClassContainer(
